716b86bc0e216b74a0a10fb466c3bf0e08b8fce2
codenamekii <magnoncro95@gmail.com>
2025-01-28 02:36:43 +0800

    baru sampe book overview

---
+++ components/BookOverview.tsx
@@ -0,0 +1,11 @@
+
+
+const BookOverview = () => {
+  return <section className="book-overview">
+    <div className="flex flex-col flex-1 gap-5">
+      <h1> Judul Sebuah Buku </h1>
+    </div>
+  </section>
+} 
+
+export default BookOverview
