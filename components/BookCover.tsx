a0a4d6240a23c8643823e1b97d909d2feb9edeea
codenamekii <magnoncro95@gmail.com>
2025-01-31 10:12:08 +0800

    nambahin book overview

---
+++ components/BookCover.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+type BookCoverVariant = 'extraSmall' | 'Small' | 'medium' | 'regular' | 'wide';
+
+const variantStyles : Record<BookCoverVariant, string> = {
+  extraSmall: 'book-cover_extra_small',
+  Small: 'book-cover_small',
+  medium: 'book-cover_medium',
+  regular: 'book-cover_regular',
+  wide: 'book-cover_wide',
+}
+
+
+interface Props {
+  classname?: string;
+  variant?: BookCoverVariant;
+  coverColor?: string;
+  coverUrl?: string;
+  coverImage?: string;
+}
+
+
+const BookCover = ({
+  classname, 
+  variant = "regular",
+  coverColor = "#012B48",
+  coverImage = "https://placehold.co/600x400.png"
+}: Props) => {
+  return (
+    <div className={cn(
+      'relative transition-all duration-300',
+      variantStyles[variant],
+      classname
+    )}
+    >
+      BOOK SIDE SVG 
+
+      <div className="absolute z-10" style={{ left: '12%', width: '87.5%', height: '88%', }}
+      >
+        <Image src={coverImage} layout="fill" objectFit="cover" alt="bookCover" className="" />
+      </div>
+    </div>
+  )
+}
+
+export default BookCover
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        // Example: Fetching from a custom domain
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        // Example: Fetching from a custom domain with a path
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
